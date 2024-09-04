f8e4f1a4e83fae9c65b0d2009ed749065d450241
Tejaskhalkar <tejas.khalkar24@sanjivani.edu.in>
2024-09-04 10:17:20 +0530

    Add files via upload

---
+++ Main (4).cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int notebook=15;
+    int pen=20;
+   float sum=(notebook*2.25)+(pen*0.75);
+    
+    cout<<"total cost of pen and notebook is :"<<sum;
+
+    return 0;
+}
